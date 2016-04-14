446b0a76f5f8d208831046853ab833e51f450374
leemason <contact@nohalfpixels.com>
2016-04-15 00:17:24 +0100

    added union support, aggregate, joins, connections, repos and models to go

--- test/builderSpec.js
+++ test/builderSpec.js
@@ -281,4 +281,244 @@ describe("Builder", function() {
         });
 
     });
+
+    describe("#insert", function () {
+
+        it("should compile a simple insert query with a json object", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .insert({
+                    first_name: 'lee',
+                    last_name: 'mason'
+                });
+
+            expect(sql.getBindings()).to.eql(['lee', 'mason']);
+            expect(sql.getInsertKeys()).to.eql(['first_name', 'last_name']);
+
+        });
+
+        it("should compile a simple insert query with an array of json object", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .insert([
+                    {
+                        first_name: 'lee',
+                        last_name: 'mason'
+                    },
+                    {
+                        first_name: 'david',
+                        last_name: 'mason'
+                    }
+                ]);
+
+            expect(sql.getBindings()).to.eql(['lee', 'mason', 'david', 'mason']);
+            expect(sql.getInsertKeys()).to.eql(['first_name', 'last_name']);
+
+        });
+
+    });
+
+    describe("#update", function () {
+
+        it("should compile a simple update query with a json object and where clauses", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .where('id', 1)
+                .update({
+                    first_name: 'lee',
+                    last_name: 'mason'
+                });
+
+            expect(sql.getBindings()).to.eql(['lee', 'mason', '1']);
+
+        });
+
+    });
+
+    describe("#delete", function () {
+
+        it("should compile a simple delete query with a where clauses", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .where('id', 1)
+                .delete();
+
+            expect(sql.getBindings()).to.eql(['1']);
+
+        });
+
+    });
+
+    describe("#truncate", function () {
+
+        it("should compile a simple truncate query", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .truncate();
+
+            expect(sql._builder.method).to.equal('Truncate');
+
+            expect(sql.getBindings()).to.eql([]);
+
+        });
+
+    });
+
+    describe("#orderBy", function () {
+
+        it("should compile add order by parameters", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .orderBy('key');
+
+            expect(sql._builder.orderBy).to.eql([{column: 'key', order: 'asc'}]);
+
+            sql.orderBy('newkey', 'desc');
+
+            expect(sql._builder.orderBy).to.eql([{column: 'key', order: 'asc'}, {column: 'newkey', order: 'desc'}]);
+        });
+
+    });
+
+    describe("#limit", function () {
+
+        it("should compile add limit parameters", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .limit(1);
+
+            expect(sql._builder.limit).to.eql([1]);
+
+            sql.limit(1, 10);
+
+            expect(sql._builder.limit).to.eql([1, 10]);
+        });
+
+    });
+
+    describe("#groupBy", function () {
+
+        it("should compile add group by parameters", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .groupBy('key');
+
+            expect(sql._builder.groupBy).to.eql([{column: 'key', order: false}]);
+
+            sql.groupBy('newkey', 'desc');
+
+            expect(sql._builder.groupBy).to.eql([{column: 'key', order: false}, {column: 'newkey', order: 'desc'}]);
+        });
+
+    });
+
+
+    describe("#havingRaw", function () {
+
+        it("should compile a simple select query with a havingRaw clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .havingRaw('`first_name` = ?', ['lee']);
+
+            expect(sql.getBindings()).to.eql(['lee']);
+
+        });
+
+    });
+
+    describe("#having", function () {
+
+        it("should compile a simple select query with a having clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .having('first_name', 'lee');
+
+            expect(sql.getBindings()).to.eql(['lee']);
+
+        });
+
+        it("should compile a simple select query with a having operator clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .having('first_name', '>=', 'lee');
+
+            expect(sql.getBindings()).to.eql(['lee']);
+
+        });
+
+        it("should compile a simple select query with a nested having clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .having(function(builder){
+                    builder
+                        .having('first_name', 'lee')
+                        .having('last_name', 'mason');
+                });
+
+            expect(sql.getBindings()).to.eql(['lee', 'mason']);
+
+        });
+
+    });
+
+    describe("#union", function () {
+
+        it("should compile a simple select query with a union clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .where('first_name', 'lee')
+                .union(function(builder){
+                    builder
+                        .table('test')
+                        .where('something', true)
+                })
+                .limit(1, 10);
+
+            expect(sql.getBindings()).to.eql(['lee', 'true']);
+
+        });
+
+    });
+
 });
--- test/compilerSpec.js
+++ test/compilerSpec.js
@@ -279,4 +279,428 @@ describe("Compiler", function() {
         });
 
     });
+
+    describe("#insert", function () {
+
+        it("should compile a simple insert query with a json object", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .insert({
+                    first_name: 'lee',
+                    last_name: 'mason'
+                });
+
+            expect(sql.toSql()).to.equal('insert into `users` (`first_name`, `last_name`) values (?, ?);');
+
+        });
+
+        it("should compile a simple insert query with an array of json object", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .insert([
+                    {
+                        first_name: 'lee',
+                        last_name: 'mason'
+                    },
+                    {
+                        first_name: 'david',
+                        last_name: 'mason'
+                    }
+                ]);
+
+            expect(sql.toSql()).to.equal('insert into `users` (`first_name`, `last_name`) values (?, ?), (?, ?);');
+
+        });
+
+    });
+
+    describe("#update", function () {
+
+        it("should compile a simple update query with a json object and where clauses", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .where('id', 1)
+                .update({
+                    first_name: 'lee',
+                    last_name: 'mason'
+                });
+
+            expect(sql.toSql()).to.equal('update `users` set `first_name` = ?, `last_name` = ? where `id` = ?;');
+
+        });
+
+    });
+
+    describe("#delete", function () {
+
+        it("should compile a simple delete query with a where clauses", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .where('id', 1)
+                .delete();
+
+            expect(sql.toSql()).to.equal('delete from `users` where `id` = ?;');
+
+        });
+
+    });
+
+    describe("#truncate", function () {
+
+        it("should compile a simple truncate query", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .truncate();
+
+            expect(sql.toSql()).to.equal('truncate `users`;');
+
+        });
+
+    });
+
+    describe("#orderBy", function () {
+
+        it("should compile a simple select query with a order by clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .select('*')
+                .where('active', true)
+                .orderBy('first_name');
+
+            expect(sql.toSql()).to.equal('select * from `users` where `active` = ? order by `first_name` asc;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .select('*')
+                .where('active', true)
+                .orderBy('first_name', 'desc');
+
+            expect(sql.toSql()).to.equal('select * from `users` where `active` = ? order by `first_name` desc;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .select('*')
+                .where('active', true)
+                .orderBy('first_name', 'desc')
+                .orderBy('last_name', 'asc');
+
+            expect(sql.toSql()).to.equal('select * from `users` where `active` = ? order by `first_name` desc, `last_name` asc;');
+
+        });
+
+        it("should compile a simple update query with a order by clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .update({
+                    active: true
+                })
+                .where('active', true)
+                .orderBy('first_name');
+
+            expect(sql.toSql()).to.equal('update `users` set `active` = ? where `active` = ? order by `first_name` asc;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .update({
+                    active: true
+                })
+                .where('active', true)
+                .orderBy('first_name', 'desc');
+
+            expect(sql.toSql()).to.equal('update `users` set `active` = ? where `active` = ? order by `first_name` desc;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .update({
+                    active: true
+                })
+                .where('active', true)
+                .orderBy('first_name', 'desc')
+                .orderBy('last_name', 'asc');
+
+            expect(sql.toSql()).to.equal('update `users` set `active` = ? where `active` = ? order by `first_name` desc, `last_name` asc;');
+
+        });
+
+        it("should compile a simple delete query with a order by clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .orderBy('first_name')
+                .delete();
+
+            expect(sql.toSql()).to.equal('delete from `users` order by `first_name` asc;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .orderBy('first_name', 'desc')
+                .delete();
+
+            expect(sql.toSql()).to.equal('delete from `users` order by `first_name` desc;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .orderBy('first_name', 'desc')
+                .orderBy('last_name', 'asc')
+                .delete();
+
+            expect(sql.toSql()).to.equal('delete from `users` order by `first_name` desc, `last_name` asc;');
+
+        });
+
+    });
+
+
+    describe("#limit", function () {
+
+        it("should compile a simple select query with a limit clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .select('*')
+                .where('active', true)
+                .limit(10);
+
+            expect(sql.toSql()).to.equal('select * from `users` where `active` = ? limit 10;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .select('*')
+                .where('active', true)
+                .limit(10, 100);
+
+            expect(sql.toSql()).to.equal('select * from `users` where `active` = ? limit 10, 100;');
+
+        });
+
+        it("should compile a simple update query with a limit clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .update({
+                    active: true
+                })
+                .where('active', true)
+                .limit(10);
+
+            expect(sql.toSql()).to.equal('update `users` set `active` = ? where `active` = ? limit 10;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .update({
+                    active: true
+                })
+                .where('active', true)
+                .limit(10, 100);
+
+            expect(sql.toSql()).to.equal('update `users` set `active` = ? where `active` = ? limit 10, 100;');
+
+        });
+
+        it("should compile a simple delete query with a limit clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .limit(10)
+                .delete();
+
+            expect(sql.toSql()).to.equal('delete from `users` limit 10;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .limit(10, 100)
+                .delete();
+
+            expect(sql.toSql()).to.equal('delete from `users` limit 10, 100;');
+
+        });
+
+    });
+
+    describe("#groupBy", function () {
+
+        it("should compile a simple select query with a group by clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .select('*')
+                .where('active', true)
+                .groupBy('first_name');
+
+            expect(sql.toSql()).to.equal('select * from `users` where `active` = ? group by `first_name`;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .select('*')
+                .where('active', true)
+                .groupBy('first_name', 'desc');
+
+            expect(sql.toSql()).to.equal('select * from `users` where `active` = ? group by `first_name` desc;');
+
+            mason = new Mason();
+
+            sql = mason
+                .table('users')
+                .select('*')
+                .where('active', true)
+                .groupBy('first_name')
+                .groupBy('last_name');
+
+            expect(sql.toSql()).to.equal('select * from `users` where `active` = ? group by `first_name`, `last_name`;');
+
+        });
+
+    });
+
+    describe("#havingRaw", function () {
+
+        it("should compile a simple select query with a havingRaw clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .havingRaw('`first_name` = ?', ['lee']);
+
+            expect(sql.toSql()).to.equal('select * from `users` having `first_name` = ?;');
+
+        });
+
+    });
+
+    describe("#having", function () {
+
+        it("should compile a simple select query with a having clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .having('first_name', 'lee');
+
+            expect(sql.toSql()).to.equal('select * from `users` having `first_name` = ?;');
+
+        });
+
+        it("should compile a simple select query with a having operator clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .having('first_name', '>=', 'lee');
+
+            expect(sql.toSql()).to.equal('select * from `users` having `first_name` >= ?;');
+
+        });
+
+        it("should compile a simple select query with a nested having clause", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .having(function(builder){
+                    builder
+                        .having('first_name', 'lee')
+                        .having('last_name', 'mason');
+                });
+
+            expect(sql.toSql()).to.equal('select * from `users` having (`first_name` = ? and `last_name` = ?);');
+
+        });
+
+    });
+
+    describe("#union", function () {
+
+        it("should compile a simple select query with union clauses", function () {
+
+            var mason = new Mason();
+
+            sql = mason
+                .select('*')
+                .from('users')
+                .where('first_name', 'lee')
+                .union(function(builder){
+                    builder
+                        .table('test')
+                        .select('*')
+                        .where('active', 'true')
+                })
+                .unionAll(function(builder){
+                    builder
+                        .table('test2')
+                        .select('*')
+                        .where('active', 'true')
+                })
+                .unionDistinct(function(builder){
+                    builder
+                        .table('test3')
+                        .select('*')
+                        .where('active', 'true')
+                })
+                .unionRaw('select * from custom')
+                .orderBy('first_name');
+
+            expect(sql.toSql()).to.equal('(select * from `users` where `first_name` = ?) union (select * from `test` where `active` = ?) union all (select * from `test2` where `active` = ?) union distinct (select * from `test3` where `active` = ?) union (select * from custom) order by `first_name` asc;');
+
+        });
+
+    });
 });
